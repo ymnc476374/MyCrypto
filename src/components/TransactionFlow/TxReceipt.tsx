fd929a189e0655205eb3e298ca319eb773c935f9
Luit Hollander <luit@hollander.email>
2020-07-28 00:33:47 +0200

    More captcha implementation

--- src/components/TransactionFlow/TxReceipt.tsx
+++ src/components/TransactionFlow/TxReceipt.tsx
@@ -58,12 +58,7 @@ import { constructCancelTxQuery, constructSpeedUpTxQuery } from '@utils/queries'
 import { makeFinishedTxReceipt } from '@utils/transaction';
 import { path } from '@vendor';
 
-import {
-  FromToAccount,
-  RecipientAccount,
-  SwapFromToDiagram,
-  TransactionDetailsDisplay
-} from './displays';
+import { FromToAccount, SwapFromToDiagram, TransactionDetailsDisplay } from './displays';
 import TxIntermediaryDisplay from './displays/TxIntermediaryDisplay';
 import { calculateReplacementGasPrice, constructSenderFromTxConfig } from './helpers';
 import { PendingTransaction } from './PendingLoader';
@@ -379,7 +374,7 @@ export const TxReceiptUI = ({
           <MembershipReceiptBanner membershipSelected={membershipSelected} />
         </div>
       )}
-      {txType !== ITxType.PURCHASE_MEMBERSHIP && txType !== ITxType.FAUCET && (
+      {txType !== ITxType.PURCHASE_MEMBERSHIP && (
         <>
           <FromToAccount
             networkId={sender.network.id}
@@ -392,16 +387,6 @@ export const TxReceiptUI = ({
               addressBookEntry: recipientContact
             }}
             displayToAddress={txType !== ITxType.DEPLOY_CONTRACT}
-          />
-        </>
-      )}
-      {txType === ITxType.FAUCET && (
-        <>
-          <RecipientAccount
-            to={{
-              address: (receiverAddress || (displayTxReceipt && displayTxReceipt.to)) as TAddress,
-              label: recipientLabel
-            }}
           />
         </>
       )}
